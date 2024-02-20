de24c1fef30c5ac3d3f5cc9f65edac333c4a99af
gustavo-lara <gustavolara63719@gmail.com>
2024-02-20 16:30:44 -0300

    começo do home

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,12 +1,19 @@
-import styles from  "./Home.module.css"
+import styles from "./Home.module.css"
+import videojs from "video.js"
+import imgInicio from "./../../img/mercedesInicio.jpg"
 
 export const Home = () => {
-    return (
-      <div className={styles.div}>
-        <p>O React Router Dom é uma biblioteca que permite criar rotas em aplicativos React. Ele é usado para criar um aplicativo de página única (SPA) com várias páginas. O React Router Dom é um pacote npm que pode ser instalado em seu projeto usando o comando npm install react-router-dom.
-Para criar rotas, você precisa importar o componente BrowserRouter do pacote react-router-dom. Em seguida, você pode usar o componente Route para definir as rotas. Cada rota é mapeada para um componente específico que será renderizado quando a rota for acessada.</p>
+  return (
+    <div className={styles.Home}>
+      <div className={styles.imgInicio}>
+        <img src={imgInicio} alt="Imagem de uma Mercedes" />
+        <div className={styles.cont}>
+        <p>A potência que pulsa</p>
+        <button>Saiba Mais</button>
         </div>
-    )
-  }
-  
-  export default Home
+      </div>
+    </div>
+  )
+}
+
+export default Home
