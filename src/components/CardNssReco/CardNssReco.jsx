80916c7fdfb367e4418b58f1561f7ce943c3bc23
gustavo-lara <gustavolara63719@gmail.com>
2024-06-22 14:45:32 -0300

    Mudança dos links do Header;

--- src/components/CardNssReco/CardNssReco.jsx
+++ src/components/CardNssReco/CardNssReco.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import styles from "./CardNssReco.module.css"
 
-const CardNssReco = () => {
+const CardNssReco = ( img, h2, p) => {
   return (
-    <div className={styles.body}></div>
+    <div className={styles.body}>
+      <img src="" alt="" />
+      <h2></h2>
+      <p></p>
+      <button></button>
+    </div>
   )
 }
 
