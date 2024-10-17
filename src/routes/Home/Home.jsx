38d29cbc6a6e266de2299fb0b1a86d2741923eb3
gustavo-lara <gustavolara63719@gmail.com>
2024-10-17 16:45:23 -0300

    Video de inicio

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,13 +1,17 @@
 import styles from "./Home.module.css"
 import videojs from "video.js"
-import imgInicio from "./../../img/mercedesInicio.jpg"
+// import imgInicio from "./../../img/mercedesInicio.jpg"
 import NssReco from "../../components/NssReco/NssReco"
+import videoInicio from "./../../videos/VideoInicio.mp4"
 
 export const Home = () => {
   return (
     <div className={styles.Home}>
-      <div className={styles.imgInicio}>
-        <img src={imgInicio} alt="Imagem de uma Mercedes" />
+      <div className={styles.TelaInicio}>
+        {/* <img src={imgInicio} alt="Imagem de uma Mercedes" /> */}
+        <video autoPlay loop muted>
+          <source src={videoInicio} type="video/mp4" />
+        </video>
       </div>
       <NssReco />
     </div>
