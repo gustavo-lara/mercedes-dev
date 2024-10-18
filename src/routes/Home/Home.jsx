c8cb17c28421574590978405de83bbd56060cac7
gustavo-lara <gustavolara63719@gmail.com>
2024-10-18 08:17:14 -0300

    Add mais um video de inicio arrumando pequeno problema de responsividade

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -2,13 +2,12 @@ import styles from "./Home.module.css"
 import videojs from "video.js"
 // import imgInicio from "./../../img/mercedesInicio.jpg"
 import NssReco from "../../components/NssReco/NssReco"
-import videoInicio from "./../../videos/VideoInicio.mp4"
+import videoInicio from "./../../videos/VideoInicio2.mp4"
 
 export const Home = () => {
   return (
     <div className={styles.Home}>
       <div className={styles.TelaInicio}>
-        {/* <img src={imgInicio} alt="Imagem de uma Mercedes" /> */}
         <video autoPlay loop muted>
           <source src={videoInicio} type="video/mp4" />
         </video>
